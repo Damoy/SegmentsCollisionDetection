8893af948923599149c6236942444039395b3938
Amandine Benza <amandine.benza@etu.unice.fr>
2018-10-31 11:58:18 +0100

    Update segment position

--- src/main/java/com/dzoum/sdc/core/model/Segment.java
+++ src/main/java/com/dzoum/sdc/core/model/Segment.java
@@ -2,24 +2,47 @@ package com.dzoum.sdc.core.model;
 
 import java.awt.Color;
 
+import com.dzoum.sdc.graphics.Screen;
+
 public class Segment {
 	
 	private Color color;
 	private int height;
 	private int width;
-	
-	public Segment(Color color, int height, int width) {
+	private int x;
+	private int y;
+		
+	public Segment(Color color, int height, int width, int x, int y) {
 		this.color = color;
 		this.height = height;
 		this.width = width;
+		this.x = x;
+		this.y = y;
 	}
-	
+
 	public void update() {
 		
 	}
 	
-	public void render() {
+	public void render(Screen s) {
 		
+	}
+	
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
 	}
 
 	public Color getColor() {
